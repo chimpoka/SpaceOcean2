e30257938f3895f5a2a6266b0073d48c46b7914e
Artem <chimpoka@list.ru>
2020-01-11 13:08:26 +0300

    Add Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private Action function;
+
+    public void LaunchRepeating(Action func, float delay, float repeatRate)
+    {
+        function = func;
+        InvokeRepeating("InvokeInternal", delay, repeatRate);
+    }
+
+    public void Launch(Action func, float delay)
+    {
+        function = func;
+        Invoke("InvokeInternal", delay);
+    }
+
+    public void StopTimer()
+    {
+        CancelInvoke();
+        function = null;
+    }
+
+    private void InvokeInternal()
+    {
+        if (function != null)
+            function.Invoke();
+    }
+}
