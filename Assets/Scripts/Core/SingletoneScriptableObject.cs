52926b85652a5a91a36cacc6c2ed46fe9cd30956
Artem <chimpoka@list.ru>
2020-02-23 00:09:03 +0300

    Fix singltone not load bug

--- Assets/Scripts/Core/SingletoneScriptableObject.cs
+++ Assets/Scripts/Core/SingletoneScriptableObject.cs
@@ -16,7 +16,7 @@ public abstract class SingletonScriptableObject<T> : ScriptableObject where T : 
         get
         {
             if (!_instance)
-                _instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+                _instance = Resources.LoadAll<T>("").FirstOrDefault();
 
             MonoBehaviour.print(_instance);
             return _instance;
